22290d8ece89ca957db3a8d7667b49b47584c26e
tjforeman <tylerforeman92@gmail.com>
2019-10-28 16:51:51 -0400

    inital commit with 5 files completed

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -2,3 +2,8 @@
 # (try doing the same thing in the JS console and see what it outputs)
 
 # YOUR CODE HERE
+
+# ** is used to take the number to the left to the power of the number on the right
+
+
+print( 2 ** 65536 )
